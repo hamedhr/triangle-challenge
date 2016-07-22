c35ff96afb15b525219178123bf6d3f1969aee1e
hamedhr <hamed.h.rafati@gmail.com>
2016-07-22 21:13:29 +0430

    Update TriangleSideNotPositive.java

--- src/main/java/com/hamedhr/exception/TriangleSideNotPositive.java
+++ src/main/java/com/hamedhr/exception/TriangleSideNotPositive.java
@@ -1,8 +1,5 @@
 package com.hamedhr.exception;
 
-/**
- * Created by hamed on 7/22/2016.
- */
 public class TriangleSideNotPositive extends RuntimeException {
     public TriangleSideNotPositive(Integer side) {
         super(String.valueOf(side));
